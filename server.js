2def37eca00408b7e46968eb3c1b1a00b1e907ba
YoonJae Kim <yoonjaekim3@gmail.com>
2023-08-24 13:04:04 -0500

    Built out 'add role' question tree and switch case

--- server.js
+++ server.js
@@ -24,41 +24,44 @@ const db = mysql.createConnection(
 const questions = [
     {   
         type: 'list',
-        name: 'table',
-        message: 'Which table would you like to access?',
-        choices: ['Departments', 'Roles', 'Employees']
+        name: 'choice',
+        message: 'What would you like to do?',
+        choices: ['View all Departments', 
+                'View all Roles', 
+                'View all Employees',
+                'Add Department',
+                'Add Role', 
+                'Add Employee', 
+                'Update an Employee Role'],
+        default: 'View all Departments',
+    },
+    {   
+        type: 'input',
+        name: 'deptName',
+        message: 'What is your new Department name?',
+        when: (answers) => answers.choice === 'Add Department',
+    },
+    {   
+        type: 'input',
+        name: 'roleTitle',
+        message: 'What is your new Role title?',
+        when: (answers) => answers.choice === 'Add Role',
+    },
+    {   
+        type: 'input',
+        name: 'roleSalary',
+        message: 'What is the salary for this new role?',
+        when: (answers) => answers.roleTitle !== undefined,
     },
     {   
         type: 'list',
-        name: 'action',
-        message: 'What would you like to add to do?',
-        choices: ['View', 'Add', 'Update']
-    },
-    {   
-        type: 'input',
-        name: 'newDept',
-        message: 'What is your new Department name?',
-        when: (answers) => answers.table === 'Departments' && answers.action === 'Add',
-    },
-    {   
-        type: 'input',
-        name: 'newRole',
-        message: 'What is your new Role name?',
-        when: (answers) => answers.table === 'Roles' && answers.action === 'Add',
-    },
-    {   
-        type: 'input',
-        name: 'newSalary',
-        message: 'What is the salary for this new role?',
-        when: (answers) => answers.newRole,
-    },
-    // {   
-    //     type: 'list',
-    //     name: 'roleDept',
-    //     message: 'What is the department for this new role?',
-    //     when: (answers) => answers.newSalary,
-    //     choices: 
-    // },
+        name: 'roleDept',
+        message: 'What is the department for this new role?',
+        when: (answers) => answers.roleSalary !== undefined,
+        choices: function () {
+            return pullDepts()
+        }
+    }
 ];
 
 app.use((req, res) => {
@@ -69,42 +72,75 @@ app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`);
 });
 
+function pullDepts() {
+    return new Promise((resolve,reject) => {
+        db.query('SELECT id, name FROM departments', function (err, results) {
+            if (err) {
+                reject(err);
+            } else {
+                const choices = results.map((dept) => ({
+                    value: dept.id,
+                    name: dept.name,
+                }));
+                resolve(choices);
+            }
+        });
+    });           
+}
+
 function init() {
-    console.log('Welcome to Employee Tracker! Please follow the prompts below');
+    console.log(`+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|E M P L O Y E E   T R A C K E R|
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+Welcome to your Employee Tracker! Please follow the prompts below:
+------------------------------------------------------------------`);
     inquirer.prompt(questions)
             .then((answers) => {
-                let table = (answers.table).toLowerCase();
-                let action = answers.action;
-
-                switch (action) {
-                    case 'View':
-                        let sql = `SELECT * FROM ${table}`;
+                let choice = answers.choice;
+                let sql = "";
+                console.log(choice)
+                switch (choice) {
+                    case 'View all Departments':
+                        sql = 'SELECT * FROM departments';
                         db.query(sql, function (err, results) {
                             console.table(results);
                         });
                         break;
-                    case 'Add':
-                        if (table === "departments") {
-                            let newDept = answers.newDept;
-                            let sql = `INSERT INTO ${table} (name)
-                            VALUES ("${newDept}")`
-                            db.query(sql, function (err, results) {
-                                console.log(`Added ${newDept} to ${table}!`);
-                            });
-                        } else if (table === "roles") {
-                            let sql = `INSERT INTO ${table} (title, salary, department_id)
-                            VALUES (${newTitle},${newSalary},${newTitle})`
-                            db.query(sql, function (err, results) {
-                                console.log(`Added ${newDept} to ${table}!`);
-                            });
-                        } 
-                        
+                    case 'View all Roles':
+                        sql = 'SELECT * FROM roles';
+                        db.query(sql, function (err, results) {
+                            console.table(results);
+                        });
                         break;
-                        
+                    case 'View all Employees':
+                        sql = 'SELECT * FROM employees'
+                        db.query(sql, function (err, results) {
+                            console.table(results);
+                        });
+                        break;
+                    case 'Add Department':
+                        let deptName = answers.deptName;
+                        sql = `INSERT INTO departments (name)
+                        VALUES ("${deptName}");`;
+                        db.query(sql, function (err, results) {
+                            console.log(`Added ${deptName} to the Departments Table!`);
+                        });
+                        break;
+                    case 'Add Role':
+                        let roleTitle = answers.roleTitle;
+                        let roleSalary = answers.roleSalary;
+                        let roleDept = answers.roleDept;
+                        console.log(roleTitle,roleSalary,roleDept);
+                        sql = `INSERT INTO roles (title, salary, department_id)
+                        VALUES ("${roleTitle}", ${roleSalary}, ${roleDept});`;
+                        db.query(sql, function (err, results) {
+                            console.log(`Added ${roleTitle} to the Roles Table!`);
+                        });
+                        break;
                     default:
                         console.log('Not built yet');
                   }
-                  console.log(action);
             })
 }
 

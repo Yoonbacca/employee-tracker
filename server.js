5765bf8e7c45b482a1e28da92470f9438f69dff1
YoonJae Kim <yoonjaekim3@gmail.com>
2023-08-24 11:28:41 -0500

    Added sequelize module and role.js model

--- server.js
+++ server.js
@@ -40,6 +40,25 @@ const questions = [
         message: 'What is your new Department name?',
         when: (answers) => answers.table === 'Departments' && answers.action === 'Add',
     },
+    {   
+        type: 'input',
+        name: 'newRole',
+        message: 'What is your new Role name?',
+        when: (answers) => answers.table === 'Roles' && answers.action === 'Add',
+    },
+    {   
+        type: 'input',
+        name: 'newSalary',
+        message: 'What is the salary for this new role?',
+        when: (answers) => answers.newRole,
+    },
+    // {   
+    //     type: 'list',
+    //     name: 'roleDept',
+    //     message: 'What is the department for this new role?',
+    //     when: (answers) => answers.newSalary,
+    //     choices: 
+    // },
 ];
 
 app.use((req, res) => {
@@ -70,9 +89,16 @@ function init() {
                             let sql = `INSERT INTO ${table} (name)
                             VALUES ("${newDept}")`
                             db.query(sql, function (err, results) {
-                                console.table(results);
+                                console.log(`Added ${newDept} to ${table}!`);
                             });
-                        }
+                        } else if (table === "roles") {
+                            let sql = `INSERT INTO ${table} (title, salary, department_id)
+                            VALUES (${newTitle},${newSalary},${newTitle})`
+                            db.query(sql, function (err, results) {
+                                console.log(`Added ${newDept} to ${table}!`);
+                            });
+                        } 
+                        
                         break;
                         
                     default:
